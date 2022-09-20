df3f8478a5542d5ea368a079722fa777e2336b2f
tjvnd <ilnotasna@gmail.com>
2022-09-20 21:59:42 +0900

    TextTest

---
+++ src/Prac/src/Editor.js
@@ -0,0 +1,46 @@
+export default function Editor({$target, initialState = {
+    title : '',
+    content: '',
+}}){
+    const $editor = document.createElement('div')
+
+    let isintialize = false
+
+    this.state = initialState
+
+    $editor.style.height = '350px'
+    $editor.style.width = '600px'
+
+    $target.appendChild($editor)
+
+    this.setState =(nextState)=>{
+        this.state = nextState
+        this.render()
+    }
+    this.render = () =>{
+        if(!isintialize){
+            $editor.innerHTML = `
+                <input type>='text' name='title' style= 'width:600px;' value='${this.state.title}'/>
+                <textarra name='content' style='width:600px;height:400px;'>${this.state.content}</textarea>
+            `
+        }
+    }
+    this.render() // 규격
+
+    $editor.addEventListener('keyup', e =>{
+        const {target} = e
+        const name = target.getAttribute('name')
+        
+        if(this.state[name]){
+            const nextState = {
+                ...this.state,
+                [name]:target.value}
+
+        }else if (e.getAttribute('name')){
+
+        }
+    })
+
+   
+
+}
