b3a08a68dfd0ff9646e8f20e800c4810fc2dc3c9
tjvnd <ilnotasna@gmail.com>
2022-09-22 22:48:38 +0900

    app.js

--- src/Prac/src/App.js
+++ src/Prac/src/App.js
@@ -1,9 +1,20 @@
 import PostsPage from "./PostsPage.js"
+import PostEditPage from './PostEditPage.js'
 
 export default function App({$target}){
 
-    const postsPage = newPostsPage({$target})
-
-    postsPage.render()
-
+    const postsPage = new PostsPage({$target})
+    const postEditPage = new PostEditPage({$target,initialState:{
+        postId:'new'
+    }})
+    this.route =()=>{
+        const {pathname} = window.location
+        
+        if(pathname ==='/'){
+            postsPage.render()
+        }else if(pathname.indexOf('/posts/')===0){
+            const [, , postId] = pathname.split('/')
+            postEditPage.setState({postId})
+        }
+    }
 }
