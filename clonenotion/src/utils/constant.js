1e2cceea42fa0b9f8351ff9f639421ecf045bd24
tjvnd <108045829+tjvnd@users.noreply.github.com>
2022-10-27 21:06:01 +0900

    Add files via upload

---
+++ clonenotion/src/utils/constant.js
@@ -0,0 +1,30 @@
+
+const ROUTER_CHANGE_EVENT_NAME = 'route-change';
+const POPSTATE_EVENT_NAME = 'popstate';
+
+const EDITOR_TITLE_CHANGE_EVENT_NAME = 'editor-title-change';
+const DOCUMENT_DELETE_EVENT_NAME = 'document-change';
+
+//왜api호출이...
+const API_USER_NAME = 'minjeong';
+const API_USER_NAME2 ='jiam'
+const API_END_POINT = 'https://kdt.roto.codes'
+const API_END_POINT1 = 'https://mwu.roto-frontend.programmers.co.kr/';
+
+const STORAGE_SET_ERROR = '로컬스토리지에 데이터를 저장할 수 없습니다.';
+const STORAGE_GET_ERROR = '로컬스토리지에서 데이터를 가져올 수 없습니다.';
+const STORAGE_REMOVE_ERROR = '로컬스토리지에서 데이터를 삭제할 수 없습니다.';
+
+export {
+  ROUTER_CHANGE_EVENT_NAME,
+  POPSTATE_EVENT_NAME,
+  EDITOR_TITLE_CHANGE_EVENT_NAME,
+  DOCUMENT_DELETE_EVENT_NAME,
+  API_USER_NAME,
+  API_END_POINT,
+  STORAGE_SET_ERROR,
+  STORAGE_GET_ERROR,
+  STORAGE_REMOVE_ERROR,
+  //API_END_POINT1,
+  //API_USER_NAME2
+};
